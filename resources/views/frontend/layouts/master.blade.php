870249c4d867d0143e76f890b2144eb6758e6b7f
tonmoy-Org <tonmoy15-5200@diu.edu.bd>
2026-08-18 01:36:09 +0600

    Fix HTTP 500 error by correcting header setting key in master.blade.php

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -1,7 +1,7 @@
 @extends('frontend.layouts.base')
 @section('base.content')
     @php
-        $headerStyle = isset($section) && isset($section['header']) ? $section['header'] : (setting('header_style') ?: 'header1');
+        $headerStyle = isset($section) && isset($section['header']) ? $section['header'] : (setting('header') ?: 'header_one');
     @endphp
     @include('frontend.layouts.header.' . $headerStyle)
     @yield('content')
